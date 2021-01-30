7778634668c4fb050aff812f8b0e1766f7fd79f5
Ryan O'Connor <ryguyoconnor93@aol.com>
2021-01-30 14:46:30 -0500

    Main Page Layout Completed

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+    public int SceneIndex = 0;
+
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("gameScene");
+    }
+
+    public void QuitGame()
+    {
+
+    }
+}
