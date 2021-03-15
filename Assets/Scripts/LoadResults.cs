b21af7e9bebac4e5cf07569934852585e0cd7783
Jared Ebels <ebelsj2@ferris.edu>
2021-03-15 00:35:54 -0400

    Endscene Interaction v1.3
    + Results screen *should* change based on player info

--- Assets/Scripts/LoadResults.cs
+++ Assets/Scripts/LoadResults.cs
@@ -4,15 +4,118 @@ using UnityEngine;
 
 public class LoadResults : MonoBehaviour
 {
+
+    public Sprite advertAccount, bpManager, salesProf, marketResearchS, mDirector, salesManage, freelancer, healthMarketer, gmTech, gmTechSales, customerProject, bdAnalyst, marketResearchA, sysArch, creativeDirect, researchDirect, mPlanner, uxDesign, uiDesign, contentStrategist, corpCommManager, prDirect;
+    public GameObject jobTitle, salary;
+    public GameObject playerName, emailInput;
+    public GameObject enterEmail, submitButton;
+    public int playerCount;
+
+    private Sprite shortcut;
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+        playerName.GetComponent<TMPro.TextMeshProUGUI>().text = GameManager.instance.players[playerCount - 1].GetComponent<PlayerInfo>().playerName;
+        shortcut = GameManager.instance.players[playerCount - 1].GetComponent<PlayerInfo>().careerChoice;
         // switch case to check sprites
+        if(shortcut == advertAccount)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Advertising Account Manager";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$62,252";
+        } else if(shortcut == bpManager)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Brand/Product Manager";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$132,840";
+        } else if(shortcut == salesProf)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Sales Professional";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$62,080";
+        } else if(shortcut == marketResearchS)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Marketing Research Specialist";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$50,500";
+        } else if (shortcut == mDirector)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Marketing Director";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$115,000";
+        } else if (shortcut == salesManage)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Sales Manager";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$74,750";
+        } else if (shortcut == freelancer)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Freelance Writer";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$49,400";
+        } else if (shortcut == healthMarketer)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Healthcare Marketer";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$84,000";
+        } else if (shortcut == gmTech)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Graphic Media Technician";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$49,000";
+        } else if (shortcut == gmTechSales)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Graphic Media Technical Sales Representative";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$49,000";
+        } else if (shortcut == customerProject)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Customer Service Project Manager";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$75,000";
+        } else if (shortcut == bdAnalyst)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Business Data Analyst";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$69,252";
+        } else if (shortcut == marketResearchA)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Market Research Analyst";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$58,000";
+        } else if (shortcut == sysArch)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "System Architect";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$105,000";
+        } else if (shortcut == creativeDirect)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Creative Director";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$132,770";
+        } else if (shortcut == researchDirect)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Research Director";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$117,000";
+        } else if (shortcut == mPlanner)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Media Planner";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$54,000";
+        } else if (shortcut == uxDesign)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "User Experience Designer";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$90,700";
+        } else if (shortcut == uiDesign)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "User Interface Designer";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$80,500";
+        } else if (shortcut == contentStrategist)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Content Strategist";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$72,500";
+        } else if (shortcut == corpCommManager)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Corporate Communications Manager";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$70,401";
+        } else if (shortcut == prDirect)
+        {
+            jobTitle.GetComponent<TMPro.TextMeshProUGUI>().text = "Public Relations Director";
+            salary.GetComponent<TMPro.TextMeshProUGUI>().text = "$82,800";
+        }
 
-        // for each case,  change the text and info based of the sprite that was saved 
+        if(GameManager.instance.players[playerCount - 1].GetComponent<PlayerInfo>().email != null)
+        {
+            emailInput.GetComponent<TMPro.TMP_InputField>().interactable = false;
+            submitButton.SetActive(false);
+            enterEmail.SetActive(false);
+            emailInput.GetComponent<TMPro.TextMeshProUGUI>().text = GameManager.instance.players[playerCount - 1].GetComponent<PlayerInfo>().email;
+        }
 
-       
-            
 
     }
 
