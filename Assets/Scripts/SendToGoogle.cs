2342f2a10a9f9d352af7d5562337b7a119e4e31f
Justin Lai <laij3@ferris.edu>
2021-03-29 03:15:19 -0400

    Data tracking in progress
    Unfinished, but layout in SendToGoogle script is there

--- Assets/Scripts/SendToGoogle.cs
+++ Assets/Scripts/SendToGoogle.cs
@@ -12,7 +12,12 @@ public class SendToGoogle : MonoBehaviour
     string Email;
     string Field;
     string Career;
-    int YTB;
+    string Color;
+    string Duration;
+    //tokens
+    string TokenA, TokenG, TokenB, TokenM, TokenD, TokenP;
+    //spaces
+    string YTB, DYK, CP, BC, Normal;
 
     [SerializeField]
     //readonly string getURL;//currently unnecessary
@@ -23,16 +28,30 @@ public class SendToGoogle : MonoBehaviour
         manager = FindObjectOfType<GameManager>();
     }
 
-    IEnumerator Post(string nameIn, string emailIn, string fieldIn)
+    IEnumerator Post(string nameIn, string emailIn, string fieldIn, string careerIn, string colorIn, string tokenAIn, string tokenGIn, string tokenBIn, string tokenMIn, string tokenDIn, string tokenPIn, string ytbIn, string dykIn, string cpIn, string bcIn, string normalIn)
     {
         List<IMultipartFormSection> form = new List<IMultipartFormSection>();
         //look for the sections in the URL that require input. Tutorial to find that value here: https://www.youtube.com/watch?v=z9b5aRfrz7M&ab_channel=LuzanBaral
         form.Add(new MultipartFormDataSection("entry.2014565725", nameIn));
         form.Add(new MultipartFormDataSection("entry.2131894653", emailIn));
         form.Add(new MultipartFormDataSection("entry.119326422", fieldIn));
-        //form.Add(new MultipartFormDataSection("entry.1833831744", careerIn));
-        //purple green red pink yellow teal
-        //marketing, graphic media management, design, business data analytics, public relations, advertising
+        form.Add(new MultipartFormDataSection("entry.2091551399", careerIn));
+        form.Add(new MultipartFormDataSection("entry.1658428665", colorIn));
+
+        //tokens
+        form.Add(new MultipartFormDataSection("entry.1868129861", tokenAIn));
+        form.Add(new MultipartFormDataSection("entry.1712409209", tokenGIn));
+        form.Add(new MultipartFormDataSection("entry.1968552602", tokenBIn));
+        form.Add(new MultipartFormDataSection("entry.594056184", tokenMIn));
+        form.Add(new MultipartFormDataSection("entry.591368335", tokenDIn));
+        form.Add(new MultipartFormDataSection("entry.276096434", tokenPIn));
+
+        //spaces
+        form.Add(new MultipartFormDataSection("entry.659316869", ytbIn));
+        form.Add(new MultipartFormDataSection("entry.600528165", dykIn));
+        form.Add(new MultipartFormDataSection("entry.1168000115", cpIn));
+        form.Add(new MultipartFormDataSection("entry.957236386", bcIn));
+        form.Add(new MultipartFormDataSection("entry.878186627", normalIn));
 
         UnityWebRequest www = UnityWebRequest.Post(postURL, form);
 
@@ -42,14 +61,13 @@ public class SendToGoogle : MonoBehaviour
         else Debug.Log("Sent player data");
     }
 
-    IEnumerator PostGameData(string ytbIn)
+    IEnumerator PostGameData(string durationIn)
     {
         //wait two seconds so the organization works in the Google Sheet
         yield return new WaitForSeconds(2);
 
         List<IMultipartFormSection> form = new List<IMultipartFormSection>();
-
-        form.Add(new MultipartFormDataSection("entry.659316869", ytbIn));
+        form.Add(new MultipartFormDataSection("entry.1382100341", durationIn));
 
         UnityWebRequest www = UnityWebRequest.Post(postURL, form);
 
@@ -70,14 +88,82 @@ public class SendToGoogle : MonoBehaviour
             Name = manager.players[i].GetComponent<PlayerInfo>().playerName;
             Email = manager.players[i].GetComponent<PlayerInfo>().email;
             Field = manager.players[i].GetComponent<PlayerInfo>().fieldChoice;
+            Color = manager.players[i].GetComponent<PlayerInfo>().avatar.name;
+            Color = Color.Substring(0, Color.IndexOf("Sit"));
+
+            Career = manager.players[i].GetComponent<PlayerInfo>().careerChoice.name;
+            switch(Career)//fill out
+            {
+                case "jc-blue1":
+                    Career = "Advertising Account Manager";
+                    break;
+                case "jc-blue2":
+                    Career = "Brand/Product Manager";
+                    break;
+                case "jc-blue3":
+                    break;
+                case "jc-blue4":
+                    break;
+                case "jc-blue5":
+                    break;
+                case "jc-blue6":
+                    break;
+                case "jc-blue7":
+                    break;
+                case "jc-blue8":
+                    break;
+                case "jc-green1":
+                    break;
+                case "jc-green2":
+                    break;
+                case "jc-green3":
+                    break;
+                case "jc-pink1":
+                    break;
+                case "jc-pink2":
+                    break;
+                case "jc-pink3":
+                    break;
+                case "jc-purple1":
+                    break;
+                case "jc-purple2":
+                    break;
+                case "jc-purple3":
+                    break;
+                case "jc-red2":
+                    break;
+                case "jc-red3":
+                    break;
+                case "jc-yellow1":
+                    break;
+                case "jc-yellow2":
+                    break;
+                case "jc-yellow3":
+                    break;
+            }//switch
+
+            
+            //tokens
+            /*TokenA;
+            TokenG;
+            TokenB;
+            TokenM;
+            TokenD;
+            TokenP;
+            //spaces
+            /*YTB;
+            DYK;
+            CP;
+            BC;
+            Normal;*/
 
             Debug.Log("Starting Coroutine");
-            StartCoroutine(Post(Name, Email, Field));
+            //StartCoroutine(Post(Name, Email, Field));
         }
 
         //game data that's been recorded. Probably need to grab from GameManager
-        YTB = 1;
-        StartCoroutine(PostGameData(YTB.ToString()));
+        Duration = 69.ToString();
+        StartCoroutine(PostGameData(Duration));
     }//Send()
 
 
